58c9a29eae82042e869a0bb58329147ec2c22cb7
João Vinny <dacostamacielf@gmail.com>
2025-09-08 11:40:01 -0300

    BUILD config updated

--- src/types/user.ts
+++ src/types/user.ts
@@ -18,6 +18,11 @@ export interface updateUserType {
     email?: string;
 }
 
-export interface ExtendedRequest extends Request {
+export interface ExtendedRequest<
+    P = any,
+    ResBody = any,
+    ReqBody = any,
+    ReqQuery = any
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
     userId?: string;
 }
